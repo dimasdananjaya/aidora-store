4fa3743c72f14bb0dc58ec5b0dd76d184849c4e5
dimasdananjaya <gededimas@gmail.com>
2021-01-10 00:26:49 +0800

    Links updated

--- resources/views/about/links.blade.php
+++ resources/views/about/links.blade.php
@@ -6,11 +6,11 @@
             <h3 class="text-center mt-4"><b>Find Our Shop In Here</b></h3>
             <div class="row">
                 <div class="col-lg-6">
-                    <a class="text-center" href="https://www.instagram.com/aidora.fashion/">
+                    <a class="text-center" href="https://meet.google.com/linkredirect?authuser=0&dest=https%3A%2F%2Fshopee.co.id%2Faidorafashion">
                         <div class="card">
                             <div class="card-body">
                                 <img class="card-img-top img-responsive pb-5 d-block mx-auto" src="{{URL::asset('resources/logo/shopee.png')}}">
-                                <a class="text-center" href="https://www.instagram.com/aidora.fashion/"><p><b>Shopee</b></p></a>
+                                <a class="text-center" href="https://meet.google.com/linkredirect?authuser=0&dest=https%3A%2F%2Fshopee.co.id%2Faidorafashion"><p><b>Shopee</b></p></a>
                             </div><!--card-body-->
                         </div><!--card-->
                     </a>
@@ -28,11 +28,11 @@
                 </div><!--col-->
 
                 <div class="col-lg-6">
-                    <a class="text-center" href="https://www.instagram.com/aidora.fashion/">
+                    <a class="text-center" href="https://meet.google.com/linkredirect?authuser=0&dest=https%3A%2F%2Fwww.facebook.com%2Faidora.fashion">
                         <div class="card">
                             <div class="card-body">
                                 <img class="card-img-top img-responsive pb-5 d-block mx-auto resize-logo" src="{{URL::asset('resources/logo/facebook.png')}}">
-                                <a class="text-center" href="https://www.instagram.com/aidora.fashion/"><p><b>Facebook Page</b></p></a>
+                                <a class="text-center" href="https://meet.google.com/linkredirect?authuser=0&dest=https%3A%2F%2Fwww.facebook.com%2Faidora.fashion"><p><b>Facebook Page</b></p></a>
                             </div><!--card-body-->
                         </div><!--card-->
                     </a>
